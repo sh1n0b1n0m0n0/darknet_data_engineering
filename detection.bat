91046a2a0f7f867cc8079c10b6b496e683a1d849
sh1n0b1n0m0n0 <48913186+sh1n0b1n0m0n0@users.noreply.github.com>
2021-07-11 16:01:30 +0300

    Add files via upload

---
+++ detection.bat
@@ -0,0 +1,2 @@
+cd C:\darknet-AlexyAB
+darknet.exe detect custom_data/yolov4-tiny-custom.cfg custom_data/yolov4-tiny-custom_last.weights D:/93/407ETR_FC01_20140402_110333_00199.jpg
---
+++ train.bat
@@ -0,0 +1,2 @@
+cd C:\darknet-AlexyAB
+darknet.exe detector train custom_data/obj.data custom_data/yolov4-tiny-custom.cfg yolov4-tiny.conv.29
